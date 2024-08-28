3f7dbf2c0babbe49b089977793730768fe34c768
itsranjithkumar <ranjithkumarms28@gmail.com>
2024-08-28 16:03:13 +0530

    problem sol

---
+++ 28 aug 2024/01.sol.py
@@ -0,0 +1,27 @@
+# Input: nums = [1,2,3,4,5,6,7], k = 3
+# Output: [5,6,7,1,2,3,4]
+# Explanation:
+# rotate 1 steps to the right: [7,1,2,3,4,5,6]
+# rotate 2 steps to the right: [6,7,1,2,3,4,5]
+# rotate 3 steps to the right: [5,6,7,1,2,3,4]
+
+# def rotate (nums, k):
+nums = [1,2,3,4,5,6,7]
+k=3
+print(nums[k+1:]+nums[:k+1])
+
+
+# Input: nums = [-1,-100,3,99], k = 2
+# Output: [3,99,-1,-100]
+# Explanation: 
+# rotate 1 steps to the right: [99,-1,-100,3]
+# rotate 2 steps to the right: [3,99,-1,-100]
+
+nums = [-1,-100,3,99]
+k = 2
+print(nums[k:]+nums[:k])
+
+
+
+    
+
